2300134a9f6cd3aef5c51d3b4dd506d886bb6f83
fady2497 <fadymezghani12345@gmail.com>
2026-01-11 23:44:45 +0100

    chore(social): mettre lURL Facebook vers Gab Arits pour deep link exact

--- src/constants/contact.ts
+++ src/constants/contact.ts
@@ -2,11 +2,10 @@ export const CONTACT = {
   email: 'contact.gabarits@gmail.com',
   phone: '+33759652867',
   social: {
-    facebook: 'https://facebook.com/gabarits.fr',
+    facebook: 'https://www.facebook.com/GabArits',
     instagram: 'https://instagram.com/gabarits.fr',
     linkedin: 'https://linkedin.com/',
   },
 }
 
 export const WHATSAPP_URL = `https://wa.me/${CONTACT.phone.replace(/[^\d]/g, '')}?text=${encodeURIComponent('Bonjour, j’ai une question sur la livraison')}`
-
